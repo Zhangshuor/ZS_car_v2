7cb97df4827324b7d8bc0b62f97add9ae2ab5536
Zhangshuor <2997605195@qq.com>
2022-03-19 21:05:28 +0800

    提交

--- Core/Src/zhangshuo_code/cppmain.cpp
+++ Core/Src/zhangshuo_code/cppmain.cpp
@@ -7,6 +7,7 @@
 ********************************************************************************/
 #include "cppmain.h"
 #include "Motor.h"
+
 //创建两个轮子对象
 Motor motor_left(&htim8,TIM_CHANNEL_1,GPIOA,GPIO_PIN_2,GPIOA,GPIO_PIN_3,1);
 Motor motor_right(&htim8,TIM_CHANNEL_2,GPIOA,GPIO_PIN_4,GPIOA,GPIO_PIN_5,-1);
